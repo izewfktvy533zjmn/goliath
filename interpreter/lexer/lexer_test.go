e7071494f2c0fb0b1bdb842112ffe089204dbdcd
Taichi Soma <izewfktvy533zjmn@gmail.com>
2020-05-24 23:51:47 +0900

    テスト文字列をファイルから読み取るように変更

--- interpreter/lexer/lexer_test.go
+++ interpreter/lexer/lexer_test.go
@@ -3,6 +3,7 @@ package lexer
 import (
     "bufio"
     "errors"
+    "fmt"
     "os"
     "testing"
     "../token"
@@ -21,27 +22,52 @@ func TestNew(test *testing.T) {
 }
 
 func TestUpdateNextChar(test *testing.T) {
-    inputText := "test" + string(WHITESPACE_AT_EOL)
+    inputText := "test"
 
-    in := bufio.NewScanner(os.Stdin)
+    fp, err := os.OpenFile("test.scm", os.O_WRONLY|os.O_CREATE, 0666)
+    if err != nil {
+        panic(err)
+    }
+    fmt.Fprint(fp, inputText)
+    fp.Close()
+
+    fp, err = os.Open("test.scm")
+    if err != nil {
+        panic(err)
+    }
+
+    in := bufio.NewScanner(fp)
     lexer := New(in)
-    lexer.Line = inputText
-    lexer.LineIndex = 0
-    lexer.NextChar = lexer.Line[0]
-
     lexer.UpdateNextChar()
 
     var expect byte
-    expect = 'e'
+    expect = 't'
     actual := lexer.NextChar
 
     if expect != actual {
         test.Errorf("%s != %s", string(expect), string(actual))
     }
+
+    fp.Close()
+
+    if err := os.Remove("test.scm"); err != nil {
+        panic(err)
+    }
 }
 
 func TestUpdateNextChar_error(test *testing.T) {
-    in := bufio.NewScanner(os.Stdin)
+    fp, err := os.OpenFile("test.scm", os.O_WRONLY|os.O_CREATE, 0666)
+    if err != nil {
+        panic(err)
+    }
+    fp.Close()
+
+    fp, err = os.Open("test.scm")
+    if err != nil {
+        panic(err)
+    }
+
+    in := bufio.NewScanner(fp)
     lexer := New(in)
 
     expect := errors.New("EndOfFileException").Error()
@@ -50,22 +76,43 @@ func TestUpdateNextChar_error(test *testing.T) {
     if expect != actual {
         test.Errorf("%s != %s", expect, actual)
     }
+
+    fp.Close()
+
+    if err := os.Remove("test.scm"); err != nil {
+        panic(err)
+    }
 }
 
 func TestGetNextToken(test *testing.T) {
-    inputText := "#t" + string(WHITESPACE_AT_EOL)
+    inputText := "#t"
+    fp, err := os.OpenFile("test.scm", os.O_WRONLY|os.O_CREATE, 0666)
+    if err != nil {
+        panic(err)
+    }
+    fmt.Fprint(fp, inputText)
+    fp.Close()
 
-    in := bufio.NewScanner(os.Stdin)
+    fp, err = os.Open("test.scm")
+    if err != nil {
+        panic(err)
+    }
+
+    in := bufio.NewScanner(fp)
     lexer := New(in)
-    lexer.Line = inputText
-    lexer.LineIndex = 0
-    lexer.NextChar = lexer.Line[0]
 
     expect := *(&token.Token{Type: token.BOOLEAN, Literal: "#t"})
     tmp, _ := lexer.GetNextToken()
+
     actual := *(tmp)
 
     if expect != actual {
         test.Errorf("%s != %s", expect, actual)
+    }
+
+    fp.Close()
+
+    if err := os.Remove("test.scm"); err != nil {
+        panic(err)
     }
 }
