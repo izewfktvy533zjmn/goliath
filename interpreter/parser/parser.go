15efcf87b78c90cc586456d91c13db998593cc14
Taichi Soma <izewfktvy533zjmn@gmail.com>
2020-06-02 21:52:47 +0900

    PraseとReadメソッドの実装

--- interpreter/parser/parser.go
+++ interpreter/parser/parser.go
@@ -7,7 +7,7 @@ import (
     "../../scheme/number"
     "../../scheme/boolean"
     "../../scheme/symbol"
-    //"../../scheme/pair"
+    "../../scheme/pair"
     "../../scheme/emptylist"
 )
 
@@ -33,19 +33,39 @@ func (parser *Parser) Parse() (interface{}, error) {
     return parser.Read()
 }
 
+func (parser *Parser) CheckNestingLevel() error {
+    if parser.NestingLevel != 0 {
+        tkn, err := parser.Lexer.GetNextToken()
+
+        if err != nil {
+            return errors.New("ParseErrorException")
+        } else {
+            parser.Token = tkn
+            return nil
+        }
+    } else {
+        return nil
+    }
+}
+
+func (parser *Parser) ReadNextToken() error {
+    tkn, err := parser.Lexer.GetNextToken()
+
+    if err != nil {
+        return errors.New("ParseErrorException")
+    } else {
+        parser.Token = tkn
+        return nil
+    }
+}
+
 func (parser *Parser) Read() (interface{}, error) {
     switch parser.Token.Type {
         case token.NUMBER:
             value, _ := parser.Token.GetValue().(int)
 
-            if parser.NestingLevel != 0 {
-                tkn, err := parser.Lexer.GetNextToken()
-
-                if err != nil {
-                    return nil, errors.New("ParseErrorException")
-                }
-
-                parser.Token = tkn
+            if err := parser.CheckNestingLevel(); err != nil {
+                return nil, err
             }
 
             return number.New(value), nil
@@ -53,14 +73,8 @@ func (parser *Parser) Read() (interface{}, error) {
         case token.BOOLEAN:
             value, _ := parser.Token.GetValue().(bool)
 
-            if parser.NestingLevel != 0 {
-                tkn, err := parser.Lexer.GetNextToken()
-
-                if err != nil {
-                    return nil, errors.New("ParseErrorException")
-                }
-
-                parser.Token = tkn
+            if err := parser.CheckNestingLevel(); err != nil {
+                return nil, err
             }
 
             return boolean.New(value), nil
@@ -68,45 +82,140 @@ func (parser *Parser) Read() (interface{}, error) {
         case token.IDENTIFIER:
             value, _ := parser.Token.GetValue().(string)
 
-            if parser.NestingLevel != 0 {
-                tkn, err := parser.Lexer.GetNextToken()
-
-                if err != nil {
-                    return nil, errors.New("ParseErrorException")
-                }
-
-                parser.Token = tkn
+            if err := parser.CheckNestingLevel(); err != nil {
+                return nil, err
             }
 
             return symbol.New(value), nil
 
         case token.LEFTPAR:
             parser.NestingLevel++
-            tkn, err := parser.Lexer.GetNextToken()
 
-            if err != nil {
+            if err := parser.ReadNextToken(); err != nil {
                 return nil, errors.New("ParseErrorException")
             }
-
-            parser.Token = tkn
 
             if parser.Token.Type == token.RIGHTPAR {
                 parser.NestingLevel--
 
-                if parser.NestingLevel != 0 {
-                    tkn, err := parser.Lexer.GetNextToken()
-
-                    if err != nil {
-                        return nil, errors.New("ParseErrorException")
-                    }
-
-                    parser.Token = tkn
+                if err := parser.CheckNestingLevel(); err != nil {
+                    return nil, err
                 }
 
                 return emptylist.New(), nil
             }
-            return nil, errors.New("ParseErrorException")
 
+            car, err := parser.Read()
+
+            if err != nil {
+                return nil, err
+            }
+
+            if parser.Token.Type == token.DOT {
+                if err := parser.ReadNextToken(); err != nil {
+                    return nil, errors.New("ParseErrorException")
+                }
+
+                cdr, err := parser.Read()
+
+                if err != nil {
+                    return nil, errors.New("ParseErrorException")
+                }
+
+                if parser.Token.Type == token.RIGHTPAR {
+                    parser.NestingLevel--
+
+                    if err := parser.CheckNestingLevel(); err != nil {
+                        return nil, err
+                    }
+
+                    ret := pair.New()
+                    ret.SetCar(car)
+                    ret.SetCdr(cdr)
+
+                    return ret, nil
+                } else {
+                    return nil, errors.New("ParseErrorException")
+                }
+            } else {
+                if parser.Token.Type == token.RIGHTPAR {
+                    parser.NestingLevel--
+
+                    if err := parser.CheckNestingLevel(); err != nil {
+                        return nil, err
+                    }
+
+                    ret := pair.New()
+                    ret.SetCar(car)
+                    ret.SetCdr(emptylist.New())
+
+                    return ret, nil
+                }
+
+                candidateRet := pair.New()
+                candidateRet.SetCar(car)
+                candidateRet.SetCdr(emptylist.New())
+                var element interface{} = candidateRet
+
+                for {
+                    sexp, err := parser.Read()
+
+                    if err != nil {
+                        return nil, err
+                    }
+
+                    if parser.Token.Type == token.RIGHTPAR {
+                        parser.NestingLevel--
+
+                        if err := parser.CheckNestingLevel(); err != nil {
+                            return nil, err
+                        }
+
+                        candidateSexp := pair.New()
+                        candidateSexp.SetCar(sexp)
+                        candidateSexp.SetCdr(emptylist.New())
+
+                        element.(*pair.Pair).SetCdr(candidateSexp)
+                        ret := candidateRet
+
+                        return ret, nil
+                    } else if parser.Token.Type == token.DOT {
+                        if err := parser.ReadNextToken(); err != nil {
+                            return nil, errors.New("ParseErrorException")
+                        }
+
+                        cdr, err := parser.Read()
+
+                        if err != nil || parser.Token.Type != token.RIGHTPAR {
+                            return nil, errors.New("ParseErrorException")
+                        }
+
+                        parser.NestingLevel--
+
+                        if err := parser.CheckNestingLevel(); err != nil {
+                            return nil, err
+                        }
+
+                        tmp := pair.New()
+                        tmp.SetCar(sexp)
+                        tmp.SetCdr(cdr)
+
+                        element.(*pair.Pair).SetCdr(tmp)
+                        ret := candidateRet
+
+                        return ret, nil
+                    } else {
+                        tmp := pair.New()
+                        tmp.SetCar(sexp)
+                        tmp.SetCdr(emptylist.New())
+                        element.(*pair.Pair).SetCdr(tmp)
+                        element = element.(*pair.Pair).GetCdr()
+                    }
+                }
+
+            }
+
+            return nil, errors.New("ParseErrorException")
 
         default:
             return nil, errors.New("ParseErrorException")
